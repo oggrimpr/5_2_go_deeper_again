be26943f367e4d14049f553af5c9ace2ae7f1bf1
oggrimpr <p.rumyantsev@overgear.com>
2021-04-05 18:26:47 +0300

    Added more test examples

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,10 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -30,14 +32,33 @@ public class TextBoxTests {
     }
 
     @Test
-    void successfulFillTest() {
+    void successfulFillTestWithVariable() {
+        String name = "alex";
+
         open("https://demoqa.com/text-box");
 
-        $("#userName").setValue("alex");
+        $("#userName").setValue(name);
         $("#userEmail").setValue("alex@qag.com");
         $("#currentAddress").setValue("current address");
         $("#permanentAddress").setValue("12345");
         $("#submit").click();
+
+        $("#name").shouldHave(text(name));
+
+    }
+    @Test
+    void successfulFillTestWithSearchInOutput() {
+        String name = "alex";
+
+        open("https://demoqa.com/text-box");
+
+        $("#userName").setValue(name);
+        $("#userEmail").setValue("alex@qag.com");
+        $("#currentAddress").setValue("current address");
+        $("#permanentAddress").setValue("12345");
+        $("#submit").click();
+
+        $("#output").shouldHave(text(name));
 
     }
 }
